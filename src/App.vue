69351be860a383a8ae71f59785be07d14ed9c0f4
pjap <pranajaya.pratama@gmail.com>
2018-08-01 14:56:12 +0700

    Create Animation Using Vue Transition and Slide In Slide Out

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr>
+        <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+        <button class="btn btn-warning" @click="show2 = !show2">Show Alert 2</button>
+        <br><br>
+        <transition name="fade">
+          <!--Can Use V-If and Can Use V-Show  -->
+          <!-- <div class="alert alert-info" v-if="show">This is some info</div> -->
+          <div class="alert alert-info" v-show="show">This is some info</div>
+        </transition>
+
+        <!-- Use Appear For On Load Page Start Animate -->
+        <transition name="fade" appear>
+          <div class="alert alert-info" v-if="show2">This is some info With Show Data 2</div>
+          <!-- <div class="alert alert-info" v-show="show">This is some info</div> -->
+        </transition>
+
+        <!-- <transition name="slide" type="animation">
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+      show2: true
+    }
+  }
+}
+</script>
+
+<style>
+  .fade-enter {
+    opacity: 0;
+  }
+  .fade-enter-active {
+    transition: opacity 2s;
+  }
+  .fade-leave {
+    /* opacity: 1; */
+  }
+  .fade-leave-active {
+    transition: opacity 2s;
+    opacity: 0;
+  }
+
+  .slide-enter {
+    opacity: 0;
+    /* transform: translateY(20px); */
+  }
+  .slide-enter-active {
+    animation: slide-in 3s linear forwards;
+    transition: opacity 0.5s;
+  }
+  .slide-leave {
+
+  }
+  .slide-leave-active {
+    animation: slide-out 3s linear forwards;
+    transition: opacity 1;
+    opacity: 0;
+  }
+
+  @@keyframes slide-in {
+    from {
+      transform: translateY(50px);
+    }
+    to {
+      transform: translateY(0px);
+    }
+  }
+
+  @@keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(50px);
+    }
+  }
+</style>
