06e91bddf4fb9450e93a62d1677669a08c9fb961
pjap <pranajaya.pratama@gmail.com>
2018-08-07 10:21:08 +0700

    Add Animation Using CSS

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,15 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
         <hr>
+        <select v-model="alertAnimation" class="form-control">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
+        <br>
         <button class="btn btn-primary" @click="show = !show">Show Alert</button>
         <button class="btn btn-warning" @click="show2 = !show2">Show Alert 2</button>
         <br><br>
-        <transition name="fade">
+        <transition :name="alertAnimation">
           <!--Can Use V-If and Can Use V-Show  -->
           <!-- <div class="alert alert-info" v-if="show">This is some info</div> -->
           <div class="alert alert-info" v-show="show">This is some info</div>
@@ -19,9 +24,12 @@
           <!-- <div class="alert alert-info" v-show="show">This is some info</div> -->
         </transition>
 
-        <!-- <transition name="slide" type="animation">
-          <div class="alert alert-info" v-if="show">This is some info</div>
-        </transition> -->
+        <transition
+          enter-class="" enter-active-class="animated bounce"
+          leave-class="" leave-active-class="animated shake">
+          <div class="alert alert-info" v-if="show">Animated Using Anime It!</div>
+        </transition>
+
       </div>
     </div>
   </div>
@@ -32,7 +40,8 @@ export default {
   data() {
     return {
       show: false,
-      show2: true
+      show2: true,
+      alertAnimation: 'fade'
     }
   }
 }
